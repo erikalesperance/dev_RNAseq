a1d4218aa85cd8ab774c36ec6dde2fcbe95cdc2b
emilyyaklich <emily.yaklich@gmail.com>
2023-12-12 12:25:49 -0500

    update

--- load_GC_data_and_sum_reps.R
+++ load_GC_data_and_sum_reps.R
@@ -2,13 +2,29 @@
 # Author: EY
 # Date: 12/9/2022
 # Version:4.1.2
-# Description: will load in the parsed gene count data
+# Description: will load in the parsed gene count data and collapse the replicates
+# This is done using DESeq
+# This is a lot of data that needs to be read in, so 100GB allocated 
+
 # set cwd
+setwd('/scratch/ely67071/sunflower_data/gene_count_data/')
 library(DESeq2)
+
+# put all of the gene_count files into a character vector in order to load them 
+gene_count_files<-dir(pattern="*\\.tab$")
+
+# read the data into a dataframe
+data_table<-data.frame(sampleName=gene_count_files,fileName=gene_count_files)
+# create a column that we want to collapse the replicates on (i.e. jus the plant #)
+remove_names<-sub("-.*",'',data_table$sampleName)
+data_table$replicates<-remove_names
 
 # load the DESeq data set
 # we read in the data using the DESeqDataSetFromHTSeqCount bc our dataset is in the same format as HTSeq data
 dds_set<-DESeqDataSetFromHTSeqCount(sampleTable=data_table,directory='/scratch/ely67071/sunflower_data/gene_count_data/',design=~0)
 
+# collapse the replicates 
+collapsed_reps<-collapseReplicates(dds_set,dds_set$replicates)
+collapsed_reps
 # save the data set 
-saveRDS(dds_set,file="/scratch/ely67071/sunflower_data/dev_deseq_dataset.Rdata")
+saveRDS(collapsed_reps,file="/scratch/ely67071/sunflower_data/collapsed_replicates_deseq_dataset.Rdata")
