c5afe42be64a7d73eb1cc5301af37a0c1fb3a3fa
erikalesperance <152518100+erikalesperance@users.noreply.github.com>
2024-12-10 11:26:36 -0600

    Update Quality_Assessment.sh

--- Quality_Assessment.sh
+++ Quality_Assessment.sh
@@ -2,9 +2,8 @@
 
 set -o pipefail
 
-module load FastQC/0.11.9-Java-11
+module load fastqc
 
-module load multiqc/1.11-GCCcore-8.3.0-Python-3.8.2
 
 if [[ -d "$QA_INPUTDIR" ]]; then
 	for f in `find $QA_INPUTDIR -name "*${SUFFIX}"`; do
@@ -17,7 +16,7 @@ if [[ -d "$QA_INPUTDIR" ]]; then
 			echo "$f is not a valid file"
 		fi
 	done
-	multiqc $QA_OUTPUTDIR -o $QA_OUTPUTDIR
+	#multiqc $QA_OUTPUTDIR -o $QA_OUTPUTDIR
 else
 	echo "$QA_INPUTDIR is not a valid directory"
 fi
